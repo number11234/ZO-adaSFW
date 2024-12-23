ed5a33be14dbed412c677cce41ee24a8a449b660
number11234 <130546850+number11234@users.noreply.github.com>
2024-12-23 23:27:13 +0800

    Delete Sensor directory

--- Sensor/func_loc_n_SenSel.m
+++
@@ -1,7 +0,0 @@
-function [f , grad]= func_loc_n_SenSel(an, x)  % 输入选中的数据，变量x
-    Atmp = an*diag(x)*an.';
-    Atmp = 0.5*(Atmp + Atmp.');
-    f = - log(det(Atmp));
-    Ainv_temp = inv(Atmp);
-    grad =  -diag(an.'*Ainv_temp*an);
-end
